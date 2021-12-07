3987dce404f00c8f47343604bc3f4be2c2d0d460
phpritesh@gmail.com <phpritesh@gmail.com>
2021-12-07 13:03:10 +0530

    serviceprovider issue fixed

--- src/SmsServiceProvider.php
+++ src/SmsServiceProvider.php
@@ -9,7 +9,6 @@ class SmsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        echo __DIR__.'/config/rksms.php'; exit;
         $this->publishes([
             __DIR__.'/config/rksms.php' => config_path('rksms.php'),
         ], 'rksms');
